6efecc7f16807b328d548a650e410d67862534eb
Zuucker <72306356+Zuucker@users.noreply.github.com>
2023-07-12 20:55:59 +0200

    adjusted camera movements :car:

--- Penguin War/Assets/Scripts/CameraMovement.cs
+++ Penguin War/Assets/Scripts/CameraMovement.cs
@@ -20,6 +20,9 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        Vector3 childGlobalPosition = this.transform.TransformPoint(mainCamera.transform.localPosition);
+
         if (Input.GetMouseButton(2))
         {
             Vector3 tmp = cameraObject.transform.position;
@@ -44,12 +47,14 @@ public class CameraMovement : MonoBehaviour
             {
                 Vector3 tmp2 = new Vector3(0, 0, 0);
                 tmp2.z = Input.mouseScrollDelta.y*5;
+                Quaternion originalRotation = Quaternion.Euler(35f, 0f, 0f);
+                Vector3 translatedPosition = childGlobalPosition + originalRotation * new Vector3(0f, 0f, tmp2.z);
 
                 if (mainCamera.transform.position.y > 8 && tmp2.z > 0)
                 {
                     mainCamera.transform.Translate(tmp2);
                 }
-                else if(mainCamera.transform.position.y < 30 && tmp2.z < 0)
+                else if(mainCamera.transform.position.y < 30 && tmp2.z < 0 && translatedPosition.z > -3)
                 {
                     mainCamera.transform.Translate(tmp2);
                 }
